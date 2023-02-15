5e7f7fbbdcb8736e854c049da4f659a0ef0bfc50
nina <n.yudkina95@yandex.ru>
2023-02-15 23:20:55 +0300

    Added Random

--- Example_3/Program.cs
+++ Example_3/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 78;
-int numberB = 2;
+﻿int numberA = new Random().Next(1,10); // 1,2,3,4 ... 9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); // 1,2,3,4 ... 9
+Console.WriteLine(numberB);
 int result = numberA+numberB;
-Console.WriteLine(result+numberB);
+Console.WriteLine(result);
