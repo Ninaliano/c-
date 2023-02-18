955e4d24b5d35d993f55df6b54903502918d4111
nina <n.yudkina95@yandex.ru>
2023-02-18 19:29:02 +0300

    Added Example_11

---
+++ Example_10/Program.cs
@@ -0,0 +1,22 @@
+﻿//             0  1  2  3  4  5  6  7  8   9
+int [] array={676,24,53,29,74,53,46,72,58,29};
+
+int n=array.Length;
+int find=55;
+int index=0;
+
+
+while (index<n)
+{
+    if(array[index]==find)
+    {
+        Console.WriteLine(index);
+        break; 
+    }
+          
+                
+         
+    // index=index+1
+    index++;
+    
+}
